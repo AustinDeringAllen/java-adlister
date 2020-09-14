4f4c372ec14ac97ab0241cb7341def3c9c908f02
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-09-14 11:15:19 -0500

    finished

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,26 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name="HelloWorldServlet",urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    private int count = 0;
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String name = req.getParameter("name"), countValue = req.getParameter("count");
+        if(countValue == null) {
+            count++;
+        } else {
+            count = Integer.parseInt(countValue);
+        }
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        if(name == null) {
+            name = "World";
+        }
+        out.println("<h1>Hello, " + name + "!</h1>");
+        out.println("<h2>This page has been viewed " + count + " times!</h2>");
     }
 }
