2851f5bec691ce8a6834b01fb00c49b7226c7de7
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-09-24 14:53:50 -0500

    finished

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -1,19 +1,36 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO: show the registration form
+        request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         // TODO: ensure the submitted information is valid
+        String email    = request.getParameter("email");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
         // TODO: create a new user based off of the submitted information
+
+        User user = new User();
+        user.setEmail(email);
+        user.setUsername(username);
+        user.setPassword(password);
+        DaoFactory.getUsersDao().insert(user);
+
         // TODO: if a user was successfully created, send them to their profile
+        response.sendRedirect("/profile");
     }
 }
---
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -0,0 +1,64 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+import config.Config;
+
+import java.sql.*;
+
+public class MySQLUsersDao implements Users {
+    private Connection connection = null;
+
+    public MySQLUsersDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        try {
+            String sql = "SELECT * FROM users WHERE username LIKE ?";
+            String search = username;
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1,search);
+            ResultSet rs = stmt.executeQuery();
+            User user = new User();
+            if(rs.next()) {
+                user.setId(rs.getLong("id"));
+                user.setUsername(rs.getString("username"));
+                user.setEmail(rs.getString("email"));
+                user.setPassword(rs.getString("password"));
+            }
+            return user;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error retrieving user.", e);
+        }
+    }
+
+    @Override
+    public Long insert(User user) {
+        try {
+            String sql = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+            PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, user.getPassword());
+
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch(SQLException e) {
+            throw new RuntimeException("Error creating user.", e);
+        }
+    }
+}
